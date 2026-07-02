03c12fdd863970904886882fe371c0aef087b794
tijana-guduric <tijanag@atomicmail.io>
2026-07-02 19:53:37 +0200

    AWS instances FinOps tags fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,14 @@ locals {
 }
 
 resource "aws_instance" "demo_server" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = var.instance_type
+  ami                         = "ami-0c02fb55956c7d316"
+  instance_type               = var.instance_type
+  associate_public_ip_address = false
 
   root_block_device {
-    volume_size = var.root_volume_size
-    volume_type = "gp3"
+    volume_size   = var.root_volume_size
+    volume_type   = "gp3"
+    encrypted     = true
 
     tags = merge(local.common_tags, {
       Name = "${var.project_name}-root-volume"
@@ -31,6 +33,7 @@ resource "aws_ebs_volume" "data_volume" {
   availability_zone = var.availability_zone
   size              = var.data_volume_size
   type              = "gp3"
+  encrypted         = true
 
   tags = merge(local.common_tags, {
     Name = "${var.project_name}-data-volume"
